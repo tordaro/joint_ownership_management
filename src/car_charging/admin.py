6d05b709d0dfc471667a3182c5f426a8b024c724
Tord Aronsen <tordaro@gmail.com>
2023-12-14 12:15:39 +0100

    Make admin

--- src/car_charging/admin.py
+++ src/car_charging/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from car_charging.models import ChargingSession, EnergyDetails
 
-# Register your models here.
+class ChargingSessionAdmin(admin.ModelAdmin):
+    list_display = ('user_full_name', 'user_email', 'start_date_time', 'energy', 'device_name', 'created_at')
+    list_filter = ('user_full_name', 'user_name', 'user_email', 'start_date_time', 'end_date_time', 'energy', 'device_name', 'created_at')
+    search_fields = ('user_full_name', "user_name")
+
+class EnergyDetailsAdmin(admin.ModelAdmin):
+    list_display = ('charging_session', 'energy', 'timestamp', 'created_at')
+    list_filter = ('charging_session', 'energy', 'timestamp', 'created_at')
+    search_fields = ('charging_session',)
+
+admin.site.register(ChargingSession, ChargingSessionAdmin)
+admin.site.register(EnergyDetails, EnergyDetailsAdmin)
