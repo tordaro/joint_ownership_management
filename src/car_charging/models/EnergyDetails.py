c68ceb96eb48fac1eecf291cc8d93d9e2f6bcccc
Tord Aronsen <tordaro@gmail.com>
2023-12-14 15:41:06 +0100

    Format models

--- src/car_charging/models/EnergyDetails.py
+++ src/car_charging/models/EnergyDetails.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class EnergyDetails(models.Model):
-    charging_session = models.ForeignKey('ChargingSession', on_delete=models.CASCADE)
+    charging_session = models.ForeignKey("ChargingSession", on_delete=models.CASCADE)
     energy = models.DecimalField(max_digits=8, decimal_places=6, verbose_name=_("Energy"))
     timestamp = models.DateTimeField(verbose_name=_("Timestamp"))
 
@@ -11,9 +12,9 @@ class EnergyDetails(models.Model):
 
     def __str__(self):
         return str(self.id)
-    
+
     class Meta:
-        db_table = 'energy_details'
-        ordering = ['timestamp']
-        verbose_name = _('Energy Detail')
-        verbose_name_plural = _('Energy Details')
+        db_table = "energy_details"
+        ordering = ["timestamp"]
+        verbose_name = _("Energy Detail")
+        verbose_name_plural = _("Energy Details")
