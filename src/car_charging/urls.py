04f7d1c7f8f5d2594298447ea5c19f0bb96b72cb
Tord Aronsen <tordaro@gmail.com>
2023-12-18 17:26:30 +0100

    Token status page

--- src/car_charging/urls.py
+++ src/car_charging/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from car_charging.views import history, index
+from car_charging.views import history, index, auth_token
+
 
 urlpatterns = [
     path("", index, name="index"),
     path("history", history.charge_history, name="history"),
+    path("token", auth_token.token_status, name="token_status"),
+    path("renew_token", auth_token.renew_token, name="renew_token"),
 ]
--- src/car_charging/views/__init__.py
+++ src/car_charging/views/__init__.py
@@ -1,4 +1,5 @@
 from .index import index
 from .history import charge_history
+from .auth_token import request_token, renew_token
 
-__all__ = ["index", "charge_history"]
+__all__ = ["index", "charge_history", "request_token", "renew_token"]
