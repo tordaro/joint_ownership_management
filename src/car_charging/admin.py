772f3f267e0c89ee4a7338388ec158bd4e91d3fb
Tord Aronsen <tordaro@gmail.com>
2023-12-27 15:23:00 +0100

    Spot price model

--- src/car_charging/admin.py
+++ src/car_charging/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
-from car_charging.models import ChargingSession, EnergyDetails
+from car_charging.models import ChargingSession, EnergyDetails, SpotPrices
+
 
 class ChargingSessionAdmin(admin.ModelAdmin):
-    list_display = ('user_full_name', 'user_email', 'start_date_time', 'energy', 'device_name', 'created_at')
-    list_filter = ('user_full_name', 'user_name', 'user_email', 'start_date_time', 'end_date_time', 'energy', 'device_name', 'created_at')
-    search_fields = ('user_full_name', "user_name")
+    list_display = ("user_full_name", "user_email", "start_date_time", "energy", "device_name", "created_at")
+    list_filter = ("user_full_name", "user_name", "user_email", "start_date_time", "end_date_time", "energy", "device_name", "created_at")
+    search_fields = ("user_full_name", "user_name")
+
 
 class EnergyDetailsAdmin(admin.ModelAdmin):
-    list_display = ('charging_session', 'energy', 'timestamp', 'created_at')
-    list_filter = ('charging_session', 'energy', 'timestamp', 'created_at')
-    search_fields = ('charging_session',)
+    list_display = ("charging_session", "energy", "timestamp", "created_at")
+    list_filter = ("charging_session", "energy", "timestamp", "created_at")
+    search_fields = ("charging_session",)
+
+
+class SpotPricesAdmin(admin.ModelAdmin):
+    list_display = ("no1", "no2", "no3", "no4", "no5", "start_time")
+    list_filter = ("no1", "no2", "no3", "no4", "no5", "start_time", "created_at")
+
 
 admin.site.register(ChargingSession, ChargingSessionAdmin)
 admin.site.register(EnergyDetails, EnergyDetailsAdmin)
+admin.site.register(SpotPrices, SpotPricesAdmin)
---
+++ src/car_charging/models/SpotPrices.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class SpotPrices(models.Model):
+    """Model definition for electric spot prices in all price areas."""
+
+    no1 = models.DecimalField(_("Spot price area 1"), max_digits=10, decimal_places=7, blank=True, null=True)
+    no2 = models.DecimalField(_("Spot price area 2"), max_digits=10, decimal_places=7, blank=True, null=True)
+    no3 = models.DecimalField(_("Spot price area 3"), max_digits=10, decimal_places=7, blank=True, null=True)
+    no4 = models.DecimalField(_("Spot price area 4"), max_digits=10, decimal_places=7, blank=True, null=True)
+    no5 = models.DecimalField(_("Spot price area 5"), max_digits=10, decimal_places=7, blank=True, null=True)
+    start_time = models.DateTimeField(_("Start time"))
+    end_time = models.DateTimeField(_("End time"))
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created At"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated At"))
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        db_table = "spot_prices"
+        ordering = ["start_time"]
+        verbose_name = _("Spot Prices")
+        verbose_name_plural = _("Spot Prices")
