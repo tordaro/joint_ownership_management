456fcb7accc45f4daf55c93726082475f64fc544
Tord Aronsen <tordaro@gmail.com>
2023-12-29 20:55:44 +0100

    Add clickable id

--- src/car_charging/admin.py
+++ src/car_charging/admin.py
@@ -3,19 +3,19 @@ from car_charging.models import ChargingSession, EnergyDetails, SpotPrices
 
 
 class ChargingSessionAdmin(admin.ModelAdmin):
-    list_display = ("user_full_name", "user_email", "start_date_time", "energy", "device_name", "created_at")
+    list_display = ("id", "user_full_name", "user_email", "start_date_time", "energy", "device_name", "created_at")
     list_filter = ("user_full_name", "user_name", "user_email", "start_date_time", "end_date_time", "energy", "device_name", "created_at")
     search_fields = ("user_full_name", "user_name")
 
 
 class EnergyDetailsAdmin(admin.ModelAdmin):
-    list_display = ("charging_session", "energy", "timestamp", "created_at")
+    list_display = ("id", "charging_session", "energy", "timestamp", "created_at")
     list_filter = ("charging_session", "energy", "timestamp", "created_at")
     search_fields = ("charging_session",)
 
 
 class SpotPricesAdmin(admin.ModelAdmin):
-    list_display = ("no1", "no2", "no3", "no4", "no5", "start_time")
+    list_display = ("id", "no1", "no2", "no3", "no4", "no5", "start_time")
     list_filter = ("no1", "no2", "no3", "no4", "no5", "start_time", "created_at")
 
 
