9c20d081a355b943b7f43485ed72f40fc92e0b28
Tord Aronsen <tordaro@gmail.com>
2024-01-21 01:04:37 +0100

    Add cost calculation

--- src/car_charging/calculate.py
+++ src/car_charging/calculate.py
@@ -1,6 +1,7 @@
 from django.utils.timezone import datetime
+from django.db.models.functions import TruncHour
 
-from car_charging.models import SpotPrices
+from car_charging.models import SpotPrices, EnergyDetails, ChargingSession
 
 
 def get_spot_prices(from_date: datetime, to_date: datetime, price_area: int) -> dict:
@@ -12,3 +13,17 @@ def get_spot_prices(from_date: datetime, to_date: datetime, price_area: int) -> 
         spot_price_map[price.hour] = getattr(price, price_area_name)
 
     return spot_price_map
+
+
+def calculate_cost(spot_price_map: dict, user_id: str, from_date: datetime, to_date: datetime) -> float:
+    user_sessions = ChargingSession.objects.filter(user_id=user_id, start_date_time__gte=from_date, end_date_time__lte=to_date)
+    energy_details = EnergyDetails.objects.filter(charging_session__in=user_sessions).annotate(hour=TruncHour("timestamp"))
+
+    total_cost = 0
+    for detail in energy_details:
+        hour = detail.hour
+        energy_used = detail.energy
+        spot_price = spot_price_map.get(hour)
+        total_cost += energy_used * spot_price
+
+    return total_cost
