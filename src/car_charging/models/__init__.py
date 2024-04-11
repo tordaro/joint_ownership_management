dafcaff16274fb572e91959b54e11324dbe1c22e
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2024-04-11 15:02:15 +0200

    new model for spot prices

--- src/car_charging/models/__init__.py
+++ src/car_charging/models/__init__.py
@@ -2,5 +2,6 @@ from .ChargingSession import ChargingSession
 from .EnergyDetails import EnergyDetails
 from .ZaptecToken import ZaptecToken
 from .SpotPrices import SpotPrices
+from .SpotPrice import SpotPrice
 
-__all__ = ["ChargingSession", "EnergyDetails", "ZaptecToken", "SpotPrices"]
+__all__ = ["ChargingSession", "EnergyDetails", "ZaptecToken", "SpotPrices", "SpotPrice"]
