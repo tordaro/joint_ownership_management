3ec972d92c8007eaaef3deab405fc6d48a8ee1e0
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2024-04-12 15:17:58 +0200

    deprecated model

--- src/car_charging/models/__init__.py
+++ src/car_charging/models/__init__.py
@@ -1,7 +1,6 @@
 from .ChargingSession import ChargingSession
 from .EnergyDetails import EnergyDetails
 from .ZaptecToken import ZaptecToken
-from .SpotPrices import SpotPrices
 from .SpotPrice import SpotPrice
 
-__all__ = ["ChargingSession", "EnergyDetails", "ZaptecToken", "SpotPrices", "SpotPrice"]
+__all__ = ["ChargingSession", "EnergyDetails", "ZaptecToken", "SpotPrice"]
