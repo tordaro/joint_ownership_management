7adedeec84e65ac792e15ce429c089ce97a10b8e
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2024-05-06 15:52:55 +0200

    complete fbr services

--- src/car_charging/fbr_services.py
+++ src/car_charging/fbr_services.py
@@ -1,8 +1,11 @@
+import logging
 import openpyxl as xl
 from pathlib import Path
 from django.utils.timezone import datetime, make_aware
 
 from car_charging.models.SpotPrice import SpotPrice
+
+logger = logging.getLogger("django")
 
 
 def parse_datetime(datetime_str: str) -> datetime:
@@ -10,7 +13,7 @@ def parse_datetime(datetime_str: str) -> datetime:
     hour_str = time_part.split("-")[0].strip()
     new_datetime_str = f"{date_part} {hour_str}"
     parsed_datetime = datetime.strptime(new_datetime_str, "%Y-%m-%d %H")
-    return parsed_datetime
+    return make_aware(parsed_datetime)
 
 
 def load_spot_prices(file_path: Path) -> None:
@@ -19,9 +22,12 @@ def load_spot_prices(file_path: Path) -> None:
     rows = list(sheet.iter_rows(values_only=True))
     spot_prices = []
     for row in rows[1:]:
-        for price_area in range(1, 6):
-            start_time = make_aware(parse_datetime(row[0]))
-            spot_price = SpotPrice(start_time=start_time, nok_per_kwh=row[1], price_area=price_area)
+        for i in range(1, 6):
+            start_time = parse_datetime(row[0])  # type: ignore
+            nok_pr_kwh = str(row[i]).replace(",", ".")
+            spot_price = SpotPrice(start_time=start_time, price_area=i, nok_pr_kwh=nok_pr_kwh)
             spot_prices.append(spot_price)
+            logger.info(f"Adding spot price for {spot_price.start_time} price area {spot_price.price_area}.")
 
     SpotPrice.objects.bulk_create(spot_prices)
+    logger.info(f"Inserted {len(spot_prices)} spot prices.")
