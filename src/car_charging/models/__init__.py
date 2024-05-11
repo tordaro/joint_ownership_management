ec0880886e7a9db53a99bf8f5019f5e4733a17c5
Tord Aronsen <tordaro@gmail.com>
2024-05-12 01:50:12 +0200

    add new models to star imports

--- src/car_charging/models/__init__.py
+++ src/car_charging/models/__init__.py
@@ -2,5 +2,7 @@ from .ChargingSession import ChargingSession
 from .EnergyDetails import EnergyDetails
 from .ZaptecToken import ZaptecToken
 from .SpotPrice import SpotPrice
+from .Costs import Costs
+from .GridPrice import GridPrice
 
-__all__ = ["ChargingSession", "EnergyDetails", "ZaptecToken", "SpotPrice"]
+__all__ = ["ChargingSession", "EnergyDetails", "ZaptecToken", "SpotPrice", "Costs", "GridPrice"]
