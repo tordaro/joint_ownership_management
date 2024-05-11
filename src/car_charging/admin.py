3f4e94d9bc7a6b6c6cb7052dd0fd52b46f735917
Tord Aronsen <tordaro@gmail.com>
2024-05-12 01:54:56 +0200

    add new models to admin

--- src/car_charging/admin.py
+++ src/car_charging/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from car_charging.models import ChargingSession, EnergyDetails, SpotPrice
+from car_charging.models import ChargingSession, EnergyDetails, SpotPrice, Costs, GridPrice
 
 
 class ChargingSessionAdmin(admin.ModelAdmin):
@@ -22,6 +22,30 @@ class SpotPricesAdmin(admin.ModelAdmin):
     list_filter = ("start_time", "created_at")
 
 
+class CostsAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        "energy",
+        "timestamp",
+        "price_area",
+        "spot_nok_pr_kwh",
+        "grid_nok_pr_kwh",
+        "spot_cost",
+        "grid_cost",
+        "created_at",
+        "updated_at",
+    )
+    list_display = ("energy_detail", "energy", "timestamp", "spot_nok_pr_kwh", "grid_nok_pr_kwh", "spot_cost", "grid_cost")
+    list_filter = ("timestamp",)
+
+
+class GridPriceAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+    list_display = ("id", "day_fee", "night_fee", "day_hour_from", "night_hour_from", "start_date", "created_at")
+    list_filter = ("start_date",)
+
+
 admin.site.register(ChargingSession, ChargingSessionAdmin)
 admin.site.register(EnergyDetails, EnergyDetailsAdmin)
 admin.site.register(SpotPrice, SpotPricesAdmin)
+admin.site.register(Costs, CostsAdmin)
+admin.site.register(GridPrice, GridPriceAdmin)
