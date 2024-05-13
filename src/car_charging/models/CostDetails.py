f0a70e6f7bd4df54c0fe8f462308758205cf9b38
Tord Aronsen <tordaro@gmail.com>
2024-05-13 16:33:06 +0200

    include usage price

--- src/car_charging/models/CostDetails.py
+++ src/car_charging/models/CostDetails.py
@@ -9,6 +9,7 @@ class CostDetails(models.Model):
     energy_detail = models.OneToOneField("EnergyDetails", on_delete=models.CASCADE, primary_key=True)
     spot_price = models.ForeignKey("SpotPrice", on_delete=models.SET_NULL, null=True)
     grid_price = models.ForeignKey("GridPrice", on_delete=models.SET_NULL, null=True)
+    usage_price = models.ForeignKey("UsagePrice", on_delete=models.SET_NULL, null=True)
 
     session_id = models.IntegerField(_("Charging session"), editable=False)
     energy = models.DecimalField(_("Energy [kWh]"), editable=False, max_digits=8, decimal_places=6)
@@ -16,11 +17,13 @@ class CostDetails(models.Model):
     price_area = models.IntegerField(_("Price area"), editable=False)
     spot_price_nok = models.DecimalField(_("Spot price per kWh [NOK/kWh]"), editable=False, max_digits=10, decimal_places=7)
     grid_price_nok = models.DecimalField(_("Grid price per kWh [NOK/kWh]"), editable=False, max_digits=10, decimal_places=7)
+    usage_price_nok = models.DecimalField(_("Usage price per kWh [NOK/kWh]"), editable=False, max_digits=10, decimal_places=7)
     user_full_name = models.CharField(verbose_name=_("User Full Name"), editable=False, max_length=100, blank=True)
     user_id = models.UUIDField(_("User ID"), editable=False, blank=True, null=True)
 
     spot_cost = models.DecimalField(_("Spot cost [NOK]"), editable=False, max_digits=11, decimal_places=7)
     grid_cost = models.DecimalField(_("Grid cost [NOK]"), editable=False, max_digits=11, decimal_places=7)
+    usage_cost = models.DecimalField(_("Usage cost [NOK]"), editable=False, max_digits=11, decimal_places=7)
     total_cost = models.DecimalField(_("Total cost [NOK]"), editable=False, max_digits=11, decimal_places=7)
 
     created_at = models.DateTimeField(_("Created At"), auto_now_add=True)
@@ -38,11 +41,14 @@ class CostDetails(models.Model):
     def set_price_area(self) -> None:
         self.price_area = self.energy_detail.charging_session.price_area
 
-    def set_spot_price(self) -> None:
-        self.spot_price_nok = self.spot_price.nok_pr_kwh
+    def set_spot_price_nok(self) -> None:
+        self.spot_price_nok = self.spot_price.get_price(self.timestamp, self.price_area)
 
-    def set_grid_price(self) -> None:
+    def set_grid_price_nok(self) -> None:
         self.grid_price_nok = self.grid_price.get_price(self.timestamp)
+
+    def set_usage_price_nok(self) -> None:
+        self.usage_price_nok = self.usage_price.get_price(self.timestamp)
 
     def set_user(self) -> None:
         self.user_id = self.energy_detail.charging_session.user_id
@@ -54,18 +60,23 @@ class CostDetails(models.Model):
     def set_spot_cost(self) -> None:
         self.spot_cost = self.energy * self.spot_price_nok
 
+    def set_usage_cost(self) -> None:
+        self.usage_cost = self.energy * self.usage_price_nok
+
     def set_total_cost(self) -> None:
-        self.total_cost = self.grid_cost + self.spot_cost
+        self.total_cost = self.grid_cost + self.spot_cost + self.usage_cost
 
     def save(self, *args, **kwargs):
         self.set_session_id()
         self.set_energy()
         self.set_timestamp()
-        self.set_spot_price()
         self.set_price_area()
-        self.set_grid_price()
+        self.set_spot_price_nok()
+        self.set_grid_price_nok()
+        self.set_usage_price_nok()
         self.set_spot_cost()
         self.set_grid_cost()
+        self.set_usage_cost()
         self.set_total_cost()
         self.set_user()
         super().save(*args, **kwargs)
