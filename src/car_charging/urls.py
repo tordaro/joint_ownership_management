4e8d6f1c78c17a033d5429ff69fdb1b6dc96a7b7
Tord Aronsen <Tord.Aronsen@skatteetaten.no>
2024-07-03 15:12:54 +0200

    init

--- src/car_charging/urls.py
+++ src/car_charging/urls.py
@@ -5,10 +5,10 @@ from car_charging.views import EnergyDetailsListView, history, index, auth_token
 app_name = "charging"
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("token", auth_token.token_status, name="token_status"),
-    path("renew_token", auth_token.renew_token, name="renew_token"),
-    path("history", history.ChargeHistoryView.as_view(), name="history"),
-    path("sessions", ChargingSessionListView.as_view(), name="session_list"),
-    path("energy", EnergyDetailsListView.as_view(), name="energy_detail_list"),
+    path("", index, name="home"),
+    # path("token", auth_token.token_status, name="token_status"),
+    # path("renew_token", auth_token.renew_token, name="renew_token"),
+    # path("history", history.ChargeHistoryView.as_view(), name="history"),
+    # path("sessions", ChargingSessionListView.as_view(), name="session_list"),
+    # path("energy", EnergyDetailsListView.as_view(), name="energy_detail_list"),
 ]
