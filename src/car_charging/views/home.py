ef118de3e99fcd9264fe87875327cac30ab56373
Tord Aronsen <tordaro@gmail.com>
2024-07-08 12:24:55 +0200

    home page for admin

--- src/car_charging/views/home.py
+++ src/car_charging/views/home.py
@@ -8,8 +8,11 @@ from car_charging.models import ChargingSession
 def home(request):
     user = request.user
     # monthly_cost = calculate_monthly_cost(user)
-    charging_sessions_count = ChargingSession.objects.filter(user_id=user.id).count()
     # spot_prices = get_spot_prices()  # Implement this function based on your logic
+    if user.is_staff:
+        charging_sessions_count = ChargingSession.objects.all().count()
+    else:
+        charging_sessions_count = ChargingSession.objects.filter(user_id=user.id)
 
     context = {
         # 'monthly_cost': monthly_cost,
