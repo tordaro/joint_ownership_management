30f760038854919e394ad3a74b732413751b7a0c
Tord Aronsen <tordaro@gmail.com>
2025-01-01 01:14:14 +0100

    a bit more permissive

--- src/car_charging/management/commands/insert_historical_fees.py
+++ src/car_charging/management/commands/insert_historical_fees.py
@@ -11,14 +11,18 @@ class Command(BaseCommand):
     help = "Convenience command that inserts historical fee prices for 01.01.2023 - 01.01.2025."
 
     def handle(self, *args, **options) -> None:
-        GridPrice.objects.create(day_fee=0.199, night_fee=0.099, day_hour_from=6, night_hour_from=22, start_date=make_aware(datetime(2023, 1, 1)))
-        GridPrice.objects.create(day_fee=0.231, night_fee=0.116, day_hour_from=6, night_hour_from=22, start_date=make_aware(datetime(2024, 1, 1)))
+        GridPrice.objects.get_or_create(
+            day_fee=0.199, night_fee=0.099, day_hour_from=6, night_hour_from=22, start_date=make_aware(datetime(2023, 1, 1))
+        )
+        GridPrice.objects.get_or_create(
+            day_fee=0.231, night_fee=0.116, day_hour_from=6, night_hour_from=22, start_date=make_aware(datetime(2024, 1, 1))
+        )
 
-        UsagePrice.objects.create(nok_pr_kwh=0.0916, start_date=make_aware(datetime(2023, 1, 1)))
-        UsagePrice.objects.create(nok_pr_kwh=0.1584, start_date=make_aware(datetime(2023, 4, 1)))
-        UsagePrice.objects.create(nok_pr_kwh=0.0951, start_date=make_aware(datetime(2024, 1, 1)))
-        UsagePrice.objects.create(nok_pr_kwh=0.1644, start_date=make_aware(datetime(2024, 4, 1)))
+        UsagePrice.objects.get_or_create(nok_pr_kwh=0.0916, start_date=make_aware(datetime(2023, 1, 1)))
+        UsagePrice.objects.get_or_create(nok_pr_kwh=0.1584, start_date=make_aware(datetime(2023, 4, 1)))
+        UsagePrice.objects.get_or_create(nok_pr_kwh=0.0951, start_date=make_aware(datetime(2024, 1, 1)))
+        UsagePrice.objects.get_or_create(nok_pr_kwh=0.1644, start_date=make_aware(datetime(2024, 4, 1)))
 
-        SpotPriceRefund.objects.create(deduction_threshold=0.0, reduction_factor=0.0, start_date=make_aware(datetime(2021, 1, 1)))
-        SpotPriceRefund.objects.create(deduction_threshold=0.73, reduction_factor=0.9, start_date=make_aware(datetime(2023, 9, 1)))
-        SpotPriceRefund.objects.create(deduction_threshold=0.0, reduction_factor=0.0, start_date=make_aware(datetime(2025, 1, 1)))
+        SpotPriceRefund.objects.get_or_create(deduction_threshold=0.0, reduction_factor=0.0, start_date=make_aware(datetime(2021, 1, 1)))
+        SpotPriceRefund.objects.get_or_create(deduction_threshold=0.73, reduction_factor=0.9, start_date=make_aware(datetime(2023, 9, 1)))
+        SpotPriceRefund.objects.get_or_create(deduction_threshold=0.0, reduction_factor=0.0, start_date=make_aware(datetime(2025, 1, 1)))
