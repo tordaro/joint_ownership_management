0124418c89eb0d98e78e3065f99f627ea55ee3c2
Tord Aronsen <tordaro@gmail.com>
2025-01-01 03:38:16 +0100

    user_name is never used

--- src/car_charging/managers/charging_session_manager.py
+++ src/car_charging/managers/charging_session_manager.py
@@ -5,7 +5,5 @@ from django.db import models
 class ChargingSessionManager(models.Manager):
     def get_unique_users(self) -> list[dict[str, str | float | None]]:
         """Return a list of dictionaries with unique user data for each unique user who has charging sessions."""
-        users_data: QuerySet = (
-            self.get_queryset().order_by("user_id").distinct("user_id").values("user_id", "user_full_name", "user_name", "user_email")
-        )
+        users_data: QuerySet = self.get_queryset().order_by("user_id").distinct("user_id").values("user_id", "user_full_name", "user_email")
         return list(users_data)
