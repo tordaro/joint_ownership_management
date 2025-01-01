5b5951bbfbe6b29f73d0981d07f354eb70e88866
Tord Aronsen <tordaro@gmail.com>
2025-01-01 01:14:54 +0100

    log creation and saving of models

--- src/car_charging/apps.py
+++ src/car_charging/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CarChargingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "car_charging"
+
+    def ready(self):
+        import car_charging.signals
